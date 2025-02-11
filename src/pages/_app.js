b68275dcfce4386bfb768706143c36714e2dd779
Br4yan-MB0 <braynmbarbosa.01@gmail.com>
2025-02-11 09:34:20 +0000

    Update _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,22 +1,9 @@
-<<<<<<< HEAD
+
 //import '../styles/globals.css'; // Ensure global styles are imported here
 import '../styles/Menu.css';
 import '../styles/LoginRegister.css';
 import '../styles/Match.css';
 
-function MyApp({ Component, pageProps }) {
+export default function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
-}
-
-=======
-//import '../styles/globals.css'; // Ensure global styles are imported here
-import '../styles/Menu.css';
-import '../styles/LoginRegister.css';
-import '../styles/Match.css';
-
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
-}
-
->>>>>>> dd736f7be0b2b91bdbcbf30cd1b7312c84b915af
-export default MyApp;
+};
