9e767ca805cb93eb47e94cf18b1d2dfee74fd226
BMB <braynmbarbosa.01@gmail.com>
2025-02-21 12:10:02 +0000

    insert index.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -3,6 +3,7 @@
 import '../styles/Menu.css';
 import '../styles/LoginRegister.css';
 import '../styles/Match.css';
+import '../styles/inic.module.css';
 
 export default function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
